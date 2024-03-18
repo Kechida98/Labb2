d1f5fe6ce609a205c43c7513386c4514eb510384
Kechida98 <kechida.ok@gmail.com>
2024-03-18 14:54:45 +0100

    Implemented task four

--- Task4labb.ino
+++ Task4labb.ino
@@ -1,4 +1,4 @@
-/*volatile uint8_t* ddrb = (volatile uint8_t*)0x24;
+volatile uint8_t* ddrb = (volatile uint8_t*)0x24;
 volatile uint8_t* portb = (volatile uint8_t*)0x25;
 volatile uint8_t* portd = (volatile uint8_t*)0x2B;
 volatile uint8_t* ddrd = (volatile uint8_t*)0x2A;
@@ -84,4 +84,4 @@ void loop() {
     counter = 0;
     Serial.println("Counter reset.");
   } 
-}*/
+}
